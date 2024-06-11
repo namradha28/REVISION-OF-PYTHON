79faec616c4b7b699101527cd99e16dcdad6e602
NAMRADHA <150675866+namradha28@users.noreply.github.com>
2024-06-11 11:44:53 +0530

    Add files via upload

---
+++ 11.06.2024/LAYOUT MANAGERS.py
@@ -0,0 +1,12 @@
+LAYOUT MANAGERS 
+#Layout Managers
+#pack(): Packs widgets into a frame.
+label.pack()
+button.pack()
+
+#grid(): Organizes widgets in a table-like structure.
+label.grid(row=0, column=0)
+entry.grid(row=0, column=1)
+
+#place(): Places widgets at an absolute position.
+label.place(x=50, y=50)
